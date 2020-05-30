7afdf083d0a391a715f8712e227d2b337cbae67d
David Felipe Medina Mayorga <dafmedinama@gmail.com>
2020-05-29 20:36:28 -0500

    Created simulation() and transaction() to manage model process. Created bins rule to generate properly histogram per simulation.

--- Money and Wealth/closed_economic_system.py
+++ Money and Wealth/closed_economic_system.py
@@ -1,26 +1,46 @@
-# Standard libraries
-
-# Third party imports
 import random as rn
 
 import numpy as np
 
-# Local application imports
 
-population = 1000
-total_money = 500000
-money_per_argent = np.full_like(np.arange(population, dtype=np.double), total_money / population)
+def simulation(money, amount, agents, time):
+    """
+    Executes a given amount of simulations.
 
-time_horizon = 100000  # Amount of time or transactions
+    :param money: Total amount of money in the system.
+    :param amount: Total simulations to be ran.
+    :param agents: Total population.
+    :param time: Time horizon. Let assume 1 tick <= 1 transaction.
+    """
 
-for i in range(0, time_horizon):
-    a = rn.randrange(0, population)
-    b = rn.randrange(0, population)
-    delta_money = round(rn.random() * money_per_argent[b],
-                        1)  # we assume agent 'b' will pay money to agent 'a' for some service
-    if delta_money <= money_per_argent[b]:  # we assume b can't have debt
+    def transaction():
+        """
+        Simulates a transaction between two agents called 'a' and 'b'. Assign new money belong to 'a' and 'b' after the
+        exchange (delta_money)
+        delta_money: Percentage of 'b' 's money to be payed to 'a' for some service.
+                     Notice that 'b' always save '1 - delta_money' of its money.
+        """
+        a = rn.randrange(0, agents)
+        b = rn.randrange(0, agents)
+        delta_money = rn.random() * money_per_argent[b]
         money_per_argent[a] = money_per_argent[a] + delta_money
         money_per_argent[b] = money_per_argent[b] - delta_money
 
+    max_money = 0.003  # Maximum amount per agent. Due to some results through multiple simulations, I found that
+    # a single agent does not posses more than the 0.3% overtime but this still must be proved somehow.
+    # Feel free to modify this value for the bins generation.
 
-# print(money_per_argent)
+    bins = np.arange(0, total_money * max_money, total_money / 10 * max_money)
+    for i in range(0, amount):
+        money_per_argent = np.full_like(np.arange(agents, dtype=np.double), money / agents)
+        for j in range(0, time):
+            transaction()
+        histogram, bin_edges = np.histogram(money_per_argent, bins)  # Result
+
+
+population = 10000
+total_money = 500000
+time_horizon = 100000
+simulations = 1
+
+simulation(total_money, simulations, population, time_horizon)
